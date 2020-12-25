b33792271956b04b6ddc6d56ece7888217504fe0
Right-Click-Code <76565060+Right-Click-Code@users.noreply.github.com>
2020-12-25 19:29:54 +0330

    Add files via upload

---
+++ claculater3.py
@@ -0,0 +1,21 @@
+# Right Click => r-click.ir
+num1 = input("Enter your Number1: ")
+num2 = input("Enter your Number2: ")
+work = input("Enter your work: ")
+
+if work is '*':
+    print(float(num1) * float(num2))
+if work is '+':
+    print(float(num1) + float(num2))
+if work is '/':
+    print(float(num1) / float(num2))
+if work is '-':
+    print(float(num1) - float(num2))
+    
+
+# result = float(num1) + float(num2)
+# print(result)
+
+# str => word , 1,2,...
+# int => 0,1,2,3,...
+# float => 1.3 , 1 , 0
